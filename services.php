4f4d7fe48438b68fc2b01a98be738b0c524b875a
Myzwer <josh@onefortyfivedesign.com>
2022-02-05 01:40:36 -0500

    :white_check_mark: Forgot to add the php template and name into the push when I committed the last batch.

--- services.php
+++ services.php
@@ -1,1 +1,19 @@
 <?php
+/**
+ * Template Name: Services
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage GL_WP
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+
+    <!-- Make content. -->
+
+<?php
+get_footer();
+
