78e8a75204e3b73a9028bfc6433a94f0cf46814f
Myzwer <josh@onefortyfivedesign.com>
2022-02-05 01:38:12 -0500

    :white_check_mark: Added all the page templates for the site.

---
+++ initiatives.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Initiatives
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage GL_WP
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+
+    <!-- Make content. -->
+
+<?php
+get_footer();
