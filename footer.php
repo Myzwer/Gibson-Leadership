c6af3e7c7583f7455b0b4d10899f2f061d6a9419
Myzwer <josh@onefortyfivedesign.com>
2022-02-15 00:30:47 -0500

    :art: Redesigned the footer. Not sure I love it, but its stable and it works for now.

--- footer.php
+++ footer.php
@@ -16,41 +16,26 @@
 
 <!--Start Footer-->
 <footer class="footer">
-    <div class="footer-left">
+    <div class="footer-outer">
 
         <!--Company Logo-->
-        <img src="https://images.squarespace-cdn.com/content/v1/575a6067b654f9b902f452f4/1552683653140-0UUVQSSUEWVC73AWAEQG/300Logo.png">
-        <div class="left-inner">
+        <img src=<?php the_field('footer_icon_wp', 'options'); ?>"">
+        <p class="mission-statement">Lead Yourself. Know Your Why. Leave a Legacy.</p>
+        <div class="footer-inner">
+            <hr>
             <!--Company Info-->
-            <h3>Fancy Company</h3>
-            <p class="phone-number">+1 234-567-8901</p>
+            <div class="footer-links">
+                <?php wp_nav_menu( array( 'theme_location' => 'footer-column-1' ) ); ?>
+                <?php wp_nav_menu( array( 'theme_location' => 'footer-column-2' ) ); ?>
+            </div>
+            <div class="footer-icons text-center">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+            </div>
+            <p class = "copyright">© <?php echo date("Y"); ?> Gibson Leadership</p>
         </div>
     </div>
 
-    <div class="footer-right">
-        <div class="right-inner">
-            <!--Wordpress Generated Link Lists-->
-            <?php wp_nav_menu( array( 'theme_location' => 'footer-column-1' ) ); ?>
-            <?php wp_nav_menu( array( 'theme_location' => 'footer-column-2' ) ); ?>
-        </div>
-
-    </div>
-    <div class="footer-bottom">
-        <div class="bot-inner">
-
-            <!--Copyright Info-->
-            <p>Some Text</p>
-            <p>© <?php echo date("Y"); ?> Website Company Name</p>
-
-            <!--Socials, icons from: https://fontawesome.com/ -->
-            <div class="footer-icons">
-            <a href="#"><i class="fa fa-facebook"></i></a>
-            <a href="#"><i class="fa fa-twitter"></i></a>
-            <a href="#"><i class="fa fa-instagram"></i></a>
-            <a href="#"><i class="fa fa-linkedin"></i></a>
-            <a href="#"><i class="fa fa-youtube"></i></a>
-        </div>
-    </div>
 </footer>
 <!--End Footer-->
 
