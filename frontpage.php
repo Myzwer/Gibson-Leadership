a1cef23f4c1c6dad7007475e5f6d019cce854f2b
Myzwer <josh@onefortyfivedesign.com>
2022-03-31 23:21:22 -0400

    :rocket: Designed the header, main section, and form section without pushing a change because I'm dumb.

--- frontpage.php
+++ frontpage.php
@@ -13,118 +13,78 @@
 
 get_header(); ?>
 
-    <video class="header-video" src="https://foothillscollective.com/wp-content/uploads/2021/04/Res-Power-Background.mp4" autoplay loop playsinline muted></video>
+    <video class="header-video"
+           src="https://foothillscollective.com/wp-content/uploads/2021/04/Res-Power-Background.mp4" autoplay loop
+           playsinline muted></video>
 
     <div class="viewport-header">
         <div class="head-container">
-            <div class="center add-padding">
-                <h1 class="text-white text-5xl pb-5">Header Title</h1>
+            <div class="center add-padding w-3/4 md:w-1/2 lg:w-1/3 mx-auto">
+                <img src="http://gibson-leadership.local/wp-content/uploads/2022/02/Full-Logo-White.png" alt="">
             </div>
-            <hr class="text-white pb-5">
-            <h2 class="text-white text-3xl ">Title</h2>
-            <h3 class="text-white text-2xl">Subtitle</h3>
+            <h2 class="text-white uppercase text-lg pt-5">Learn how to lead yourself</h2>
+            <button class="mx-auto lg:mx-0 hover:underline bg-orange text-white font-bold rounded-full py-2 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                Begin Your Journey
+            </button>
         </div>
     </div>
 
-    <div class="bg-white pb-10">
-        <div class="m-4 md:m-10 lg:max-w-4xl lg:text-center lg:mx-auto pt-10">
-            <div class="grid">
-                <div class=" grid-cols-12">
+
+    <div class="bg-white">
+        <div class="bg-no-repeat bg-scroll bg-cover relative pb-8"
+             style="background: linear-gradient(
+  rgba(245, 235, 232, 0.45),
+  rgba(245, 235, 232, 0.45)
+), url('http://gibson-leadership.local/wp-content/uploads/2022/02/background.png') center center;">
+            <div class="m-4 md:m-10 lg:max-w-6xl lg:text-center lg:mx-auto pt-10">
+                <div class="grid grid-cols-12">
                     <div class="col-span-12">
-                        <div class="text-center md:text-left mb-1">
-                            <h1>Color Pod</h1>
-                            <p>All fields optional</p>
+                        <div class="text-left mb-1">
+                            <h1 class="text-black text-4xl pb-3">The most important person you will ever lead is
+                                YOU.</h1>
+                            <p class="text-xl">Gibson Leadership exists to build leaders. It is simple. If you want to
+                                start something that makes an impact, you need to learn how to do 4 things: </p>
+                            <ul class="text-black text-2xl pb-3">
+                                <li>1. Lead Yourself</li>
+                                <li>2. learn your strengths</li>
+                                <li>3. Launch your why</li>
+                                <li>4. leave a legacy</li>
+                            </ul>
+                            <p class="text-xl pb-5">Every relationship and every result flows out of those four main
+                                quadrants of a leader's life. No matter where you find yourself on the leadership
+                                journey today, it is never too late to become a healthy leader who leaves a legacy.</p>
+                            <button class="mx-auto lg:mx-0 hover:underline bg-orange text-white font-bold rounded-full py-2 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                                Discover More
+                            </button>
                         </div>
-                        <hr>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
 
-            <div class="grid grid-cols-12 gap-4 mt-6">
-                <div class="col-span-12 lg:col-span-4 card-gradient-1 rounded-xl shadow-xl">
-                    <div class="text-center p-4">
-                        <h2 class = "font-bold text-2xl">Title</h2>
-                        <h4 class = "font-semibold">Subtitle</h4>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
-                        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Call To Action
-                        </button>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-4 card-gradient-2 rounded-xl shadow-xl">
-                    <div class="text-center p-4">
-                        <h2 class = "font-bold text-2xl">Title</h2>
-                        <h4 class = "font-semibold">Subtitle</h4>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
-                        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Call To Action
-                        </button>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-4 card-gradient-3 rounded-xl shadow-xl">
-                    <div class="text-center p-4">
-                        <h2 class = "font-bold text-2xl">Title</h2>
-                        <h4 class = "font-semibold">Subtitle</h4>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
-                        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Call To Action
-                        </button>
-                    </div>
+    <div class="bg-blue relative">
+        <div class="grid grid-cols-12 md:h-3/6">
+            <div class="col-span-6">
+                <div class="bg-no-repeat bg-scroll bg-cover relative pb-8 h-full"
+                     style="background: linear-gradient(
+  rgba(0, 0, 0, 0.45),
+  rgba(0, 0, 0, 0.45)
+), url('http://gibson-leadership.local/wp-content/uploads/2022/03/Homepage-Header.jpg') top center;">
                 </div>
             </div>
 
-            <div class="grid grid-cols-12 mt-20">
-                <div class="col-span-12">
-                    <div class="text-center md:text-left mb-1">
-                        <h1>PDF Download (four-up)</h1>
-                        <p>This configuration always shows four PDF's on one row.</p>
-                    </div>
-                    <hr>
-                </div>
-            </div>
-
-
-            <div class="grid grid-cols-12 mt-5 gap-6">
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class = "rounded-full" src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="">
-                        </div>
-                        <h2 class = "font-bold text-2xl">PDF Title</h2>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class = "rounded-full" src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="">
-                        </div>
-                        <h2 class = "font-bold text-2xl">PDF Title</h2>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class = "rounded-full" src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="">
-                        </div>
-                        <h2 class = "font-bold text-2xl">PDF Title</h2>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class = "rounded-full" src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="">
-                        </div>
-                        <h2 class = "font-bold text-2xl">PDF Title</h2>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
-                    </div>
+            <div class="col-span-6 relative px-10 md:py-40">
+                <div class="">
+                    <h2 class="uppercase text-white text-5xl">Get your free resource today</h2>
+                    <h3 class="uppercase text-white text-3xl">5 ways you can be a better leader <span
+                                class="text-orange">today</span></h3>
+                    <?php if (have_posts()) : while (have_posts()) : the_post();
+                        the_content();
+                    endwhile;
+                    else: ?>
+                        <p>Sorry, no posts matched your criteria.</p>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
