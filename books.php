b9e3d65ce820e864b1b6a4e2087c37b6fc39f761
Myzwer <josh@onefortyfivedesign.com>
2022-04-07 23:24:05 -0400

    :rocket: Completed the initial design of the books page.

--- books.php
+++ books.php
@@ -12,7 +12,112 @@
 get_header(); ?>
 
 
-    <!-- Make content. -->
+    <div class="bg-white relative">
+        <div class="bg-no-repeat bg-scroll bg-cover relative pb-10"
+             style="background: linear-gradient(
+              rgba(245, 235, 232, 0.45),
+              rgba(245, 235, 232, 0.45)
+            ), url('http://gibson-leadership.local/wp-content/uploads/2022/02/background.png') center center; height: 50vh;">
+
+            <div class="absolute top-10 md:-top-5 -left-10">
+                <img class = "opacity-20 " src="http://gibson-leadership.local/wp-content/uploads/2022/02/cropped-Logo-Orange.png" alt="">
+            </div>
+
+
+                    <div class="content-middle px-5">
+                    <h1 class="uppercase text-black font-bold text-4xl md:text-5xl">GL Projects</h1>
+                    <h2 class="uppercase text-black text-2xl md:text-3xl pt-3">Short one sentence CTA of the projects of Gibson Leadership</h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-blue py-16 relative z-10">
+        <div class="lg:max-w-6xl lg:text-center lg:mx-auto py-10 px-5">
+            <div class="grid grid-cols-12 gap-4 md:gap-10">
+                <div class="col-span-12 bg-green p-10 rounded-xl shadow-xl">
+                    <div class="grid grid-cols-12">
+                        <div class="col-span-12 md:col-span-4">
+                            <img class = "rounded-xl" src="http://gibson-leadership.local/wp-content/uploads/2022/04/Bearden-Header-1.jpg" alt="">
+                        </div>
+
+                        <div class="col-span-12 md:col-span-8 text-left">
+                            <h1 class="text-black text-3xl pb-3 uppercase">Multisite</h1>
+                            <p class = "pb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut dicta dolor error laudantium libero molestiae necessitatibus nisi pariatur quas, quasi quo sit sunt temporibus vel, velit vitae. Accusamus, beatae?</p>
+                            <button class="mx-auto lg:mx-0 hover:underline bg-orange text-white font-bold rounded-md py-2 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                                Learn More
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-span-12 bg-green p-10 rounded-xl shadow-xl">
+                    <div class="grid grid-cols-12">
+                        <div class="col-span-12 md:col-span-4">
+                            <img class = "rounded-xl" src="http://gibson-leadership.local/wp-content/uploads/2022/04/Bearden-Header-1.jpg" alt="">
+                        </div>
+
+                        <div class="col-span-12 md:col-span-8 text-left">
+                            <h1 class="text-black text-3xl pb-3 uppercase">Multisite</h1>
+                            <p class = "pb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut dicta dolor error laudantium libero molestiae necessitatibus nisi pariatur quas, quasi quo sit sunt temporibus vel, velit vitae. Accusamus, beatae?</p>
+                            <button class="mx-auto lg:mx-0 hover:underline bg-orange text-white font-bold rounded-md py-2 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                                Learn More
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-span-12 bg-green p-10 rounded-xl shadow-xl">
+                    <div class="grid grid-cols-12">
+                        <div class="col-span-12 md:col-span-4">
+                            <img class = "rounded-xl" src="http://gibson-leadership.local/wp-content/uploads/2022/04/Bearden-Header-1.jpg" alt="">
+                        </div>
+
+                        <div class="col-span-12 md:col-span-8 text-left">
+                            <h1 class="text-black text-3xl pb-3 uppercase">Multisite</h1>
+                            <p class = "pb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut dicta dolor error laudantium libero molestiae necessitatibus nisi pariatur quas, quasi quo sit sunt temporibus vel, velit vitae. Accusamus, beatae?</p>
+                            <button class="mx-auto lg:mx-0 hover:underline bg-orange text-white font-bold rounded-md py-2 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                                Learn More
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-span-12 bg-green p-10 rounded-xl shadow-xl">
+                    <div class="grid grid-cols-12">
+                        <div class="col-span-12 md:col-span-4">
+                            <img class = "rounded-xl" src="http://gibson-leadership.local/wp-content/uploads/2022/04/Bearden-Header-1.jpg" alt="">
+                        </div>
+
+                        <div class="col-span-12 md:col-span-8 text-left">
+                            <h1 class="text-black text-3xl pb-3 uppercase">Multisite</h1>
+                            <p class = "pb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut dicta dolor error laudantium libero molestiae necessitatibus nisi pariatur quas, quasi quo sit sunt temporibus vel, velit vitae. Accusamus, beatae?</p>
+                            <button class="mx-auto lg:mx-0 hover:underline bg-orange text-white font-bold rounded-md py-2 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                                Learn More
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-span-12 bg-green p-10 rounded-xl shadow-xl">
+                    <div class="grid grid-cols-12">
+                        <div class="col-span-12 md:col-span-4">
+                            <img class = "rounded-xl" src="http://gibson-leadership.local/wp-content/uploads/2022/04/Bearden-Header-1.jpg" alt="">
+                        </div>
+
+                        <div class="col-span-12 md:col-span-8 text-left">
+                            <h1 class="text-black text-3xl pb-3 uppercase">Multisite</h1>
+                            <p class = "pb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut dicta dolor error laudantium libero molestiae necessitatibus nisi pariatur quas, quasi quo sit sunt temporibus vel, velit vitae. Accusamus, beatae?</p>
+                            <button class="mx-auto lg:mx-0 hover:underline bg-orange text-white font-bold rounded-md py-2 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                                Learn More
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 
 <?php
 get_footer();
