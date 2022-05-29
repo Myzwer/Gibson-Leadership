b77dc4d914db20731aed18f4d90dd4a84c47b601
Myzwer <josh@onefortyfivedesign.com>
2022-05-29 09:46:45 -0400

    :rocket: Added a resource Confirmation page that all the resources can land on after forms are filled out.

---
+++ resource-confirmation.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template Name: Resource Confirmation
+ *
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ *
+ * @package WordPress
+ * @subpackage GL_WP
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+    <div class="grid grid-cols-12">
+        <div class="col-span-12 md:col-span-6 text-center">
+            <div class="md:text-left md:mx-10">
+
+
+                <p class="md:mt-10 text-xl"><?php the_field('title'); ?></p>
+                <h1 class="uppercase text-2xl md:text-3xl"><?php the_field('subtitle'); ?></h1>
+                <p><?php the_field('instructions'); ?></p>
+                <?php if (have_rows('resource_button')): ?>
+                    <?php while (have_rows('resource_button')): the_row(); ?>
+                        <a href="<?php the_sub_field('download_link') ?>" download>
+                            <button class="mx-auto mt-3 lg:mx-0 hover:underline bg-orange text-white font-bold rounded-full py-2 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                                <?php the_sub_field('button_text') ?>
+                            </button>
+                        </a>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+            </div>
+        </div>
+
+        <div class="col-span-12 md:col-span-6">
+            <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
+                rgba(0, 0, 0, 0.45),
+                rgba(0, 0, 0, 0.45)
+                ), url('<?php the_field('background_image'); ?>') top center;
+                height: 80vh;">
+            </div>
+        </div>
+    </div>
+
+<?php get_footer();
