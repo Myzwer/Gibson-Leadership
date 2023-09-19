d776cec2fea0c06cbf4239f4ebfbe289f1106eba
Myzwer <josh@onefortyfivedesign.com>
2023-09-18 22:13:17 -0400

    :bug: Fixed a navbar issue where the icon wasn't linking back to homepage on mobile and added a quality of life upgrade to mobile slide navigation.

--- assets/src/js/frontend.js
+++ assets/src/js/frontend.js
@@ -5,35 +5,58 @@ import "../sass/frontend.scss";
   // Begin jQuery
   $(function () {
     // DOM ready
+
     // If a link has a dropdown, add sub menu toggle.
     $("nav ul li a:not(:only-child)").click(function (e) {
-      $(this).siblings(".nav-dropdown").toggle();
+      // Remove "active-dropdown" class from other anchor elements
+      $("nav ul li a").not(this).removeClass("active-dropdown");
+
+      // Toggle the "active-dropdown" class on the clicked anchor element
+      $(this).toggleClass("active-dropdown");
+
+      // Toggle the visibility of the sibling dropdown
+      const mediaQuery = window.matchMedia("(max-width: 64em)");
+
+      // Check the screen size using the media query
+      if (mediaQuery.matches) {
+        // Mobile: Use slideToggle()
+        $(this).siblings(".nav-dropdown").slideToggle();
+      } else {
+        // Desktop: Use toggle()
+        $(this).siblings(".nav-dropdown").toggle();
+      }
+
       // Close one dropdown when selecting another
       $(".nav-dropdown").not($(this).siblings()).hide();
+
       e.stopPropagation();
     });
-    // Clicking away from dropdown will remove the dropdown class
+
+    // Clicking away from dropdown will remove the dropdown class and active-dropdown class
     $("html").click(function () {
       $(".nav-dropdown").hide();
+      $("nav ul li a:not(:only-child)").removeClass("active-dropdown");
     });
+
     // Toggle open and close nav styles on click
     $("#nav-toggle").click(function () {
       $("nav ul").slideToggle();
     });
+
     // Hamburger to X toggle
     $("#nav-toggle").on("click", function () {
       this.classList.toggle("active");
     });
-  }); // end DOM ready
+  });
 })(jQuery); // end jQuery
 
 // Add modal id and class for... well the modal.
-jQuery(document).ready(function ($) {
+/* jQuery(document).ready(function ($) {
   $(".nav-list > li:last-child").attr("id", "one");
 });
 jQuery(document).ready(function ($) {
   $(".nav-list > li:last-child").attr("class", "button");
-});
+}); */
 
 // MODAL POPUP
 // (function ($) {
